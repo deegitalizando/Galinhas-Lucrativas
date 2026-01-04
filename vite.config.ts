348bf3263b4b6e50da251459c6f6182760e6c0d9
deegitalizando <diegocarreiroonline@gmail.com>
2026-01-03 22:24:49 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // ESSA LINHA É A MAIS IMPORTANTE
+  base: './', 
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
   server: {
     port: 8080,
     host: true
