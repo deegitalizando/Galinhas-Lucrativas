954a8ab763f3e6b825bb3c65a482da27f6ba5a82
deegitalizando <diegocarreiroonline@gmail.com>
2026-01-03 21:57:42 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8080,
+    host: true
+  },
   preview: {
     port: 8080,
-    strictPort: true,
     host: true,
-    allowedHosts: true // Esta linha libera o acesso de qualquer endereço
-  }
+    allowedHosts: true
+  },
+  base: './' // Isso garante que o site encontre os arquivos em qualquer link
 })
